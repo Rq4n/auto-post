0b7aa213f14ffcb5f649dd1e82e366f934372ba4
Rq4n <ryanlimabatista2@gmail.com>
2026-08-09 19:19:57 -0300

    feat socials database schema

--- backend/db/migrations/00003_posts_sessions.sql
+++ backend/db/migrations/00003_posts_sessions.sql
@@ -4,8 +4,12 @@ CREATE TABLE IF NOT EXISTS social_connections (
   user_ID UUID REFERENCES user(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  
+  CONSTRAINT unique_user_provider UNIQUE (user_id, provider),
+  CONSTRAINT valid_provider CHECK (provider IN ('linkedin', 'twitter'))
 
-)
+);
 
 -- +goose Down
 DROP TABLE IF NOT EXISTS social_connections;
+  
